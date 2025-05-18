2484e8c319c1c7eeb279989803f814119903621f
Fabio Chu <fabiochuortega@gmail.com>
2025-05-18 17:38:26 -0500

    cambio en el titulo y final

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ hijo.forEach((h) => {
         h.style.backgroundColor = 'blue'
     }
 })
+
+const h1 = document.querySelector('h1')
+h1.style.color = 'white'
+h1.style.backgroundColor = 'black'
+h1.style.padding = '10px'
+h1.style.borderRadius = '10px'
